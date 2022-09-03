29b9b7c0ca081e62b7b3340bc6b5be1cb1e10978
Youssef kafa <youssef.kafa19941@gmail.com>
2022-09-03 16:02:46 -0700

    extended slice

---
+++ strings.py
@@ -0,0 +1,12 @@
+#the extended slice syntax is [start:stop:step], start index is included but stop index is not
+str="python"
+#print(str[1:2]) # items from index 1 through index 2
+#output y
+print(str[:2]) #items from index 0 through 2
+#output py
+print(str[:-2])
+#output pyth
+print(str[::2])
+#output pto
+print(str[::-1]) #reverse
+#output nohtyp
