78b2ced02956111f1df185613a994f659e68e1ac
Youssef kafa <youssef.kafa19941@gmail.com>
2022-09-18 10:53:25 -0700

    yield keyword

--- yield.py
+++ yield.py
@@ -4,3 +4,7 @@ def count(times):
 if __name__=='__main__':
    for x in count(10):
     print(x)
+
+"""
+yield : Yield is a keyword in Python that is used to return from a function without destroying the states of its local variable and when the function is called, the execution starts from the last yield statement. Any function that contains a yield keyword is termed a generator. Hence, yield is what makes a generator.
+"""
