7ebf7a6f9b8a81a522184e17d5cbb48acba8d632
Youssef kafa <youssef.kafa19941@gmail.com>
2022-09-19 11:19:40 -0700

    title and capitalize

---
+++ capitalizeTitle.py
@@ -0,0 +1,3 @@
+print("hello world".capitalize()) #for only the first word
+print("hello world".title()) #captilaze every word
+print("hello world 3note".title()) #note that it will update and capitalize the note word also
