251b541a3ef04529b684957f48ec4db0ba06a70e
Youssef kafa <youssef.kafa19941@gmail.com>
2022-09-20 13:48:27 -0700

    some string format

--- strings.py
+++ strings.py
@@ -12,3 +12,13 @@ print(str[::-1]) #reverse
 #output nohtyp
 print(str[:])
 #output python
+name='youssef'
+#print("Hey, %s!" % name) 
+#prints Hey, youssef!
+first='youssef'
+last='kafa'
+#print("Hey, %s %s!" %(first,last))
+#prints Hey, youssef kafa!
+#print("x",2,"y",3,"z") #prints x 2 y 3 z (space between)
+#print('%d' %('youssef')) #error a real number is required
+#print('%d' %(20.00)) #prints 20
