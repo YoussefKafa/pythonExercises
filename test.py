f462da2c109097b2f70ad828c7751314dc54ea2c
Youssef kafa <youssef.kafa19941@gmail.com>
2022-10-01 11:45:05 -0700

    linear search

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-if __name__=='__main__':
-    for i in range(10,0,-1):
-        print(i)
+def staircase(n):
+    for i in range(1,n+1):
+            str1=getChrs(' ',n-i)
+            str2=getChrs('#',i)
+            print(str1+str2)
+def getChrs(chr,n):
+    h=''
+    for i in range(n):
+        h+=chr
+    return h
+if __name__ == '__main__':
+    n = int(input().strip())
+    staircase(n)
