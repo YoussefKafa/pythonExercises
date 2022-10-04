8d808a718f47de6fc628217383ed550fa3a28efc
Youssef kafa <youssef.kafa19941@gmail.com>
2022-10-03 18:50:58 -0700

    stack implementation 1

---
+++ DataStructures/stacks.py
@@ -0,0 +1,16 @@
+"""
+it is an array or a list , it is a linear data structure that stores items
+in a last-in/first-out or first-in/last-out manner.
+in stacks , a new element is added to one end and an element is removed from that end only.
+the insert and delete operations are often called push and pop.
+"""
+
+#implementation using lists
+stack=[]
+stack.append(1)
+stack.append(2)
+stack.append(3)
+#print(stack)
+stack.pop()
+stack.pop()
+#print(stack)
