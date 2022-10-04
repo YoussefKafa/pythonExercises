c609dc8fdc1b8a47caef3d549382515ac92a940e
Youssef kafa <youssef.kafa19941@gmail.com>
2022-10-03 20:07:39 -0700

    basic linkedList with print function

---
+++ DataStructures/linkedLists.py
@@ -0,0 +1,26 @@
+#first we create a node class with datavalue=None in init and nextvalue=None
+class Node:
+    def __init__(self,dataValue=None):
+        self.dataValue=dataValue
+        self.nextValue=None
+#next we create a linkedList class with a headValue=None
+class LinkedList:
+    def __init__(self):
+        self.headValue=None
+    def printIt(self):
+        printValue=self.headValue
+        while printValue is not None:
+            print(printValue.dataValue)
+            printValue=printValue.nextValue
+
+linkedList1=LinkedList()
+#create the first node or the head node
+linkedList1.headValue=Node("Sun")
+#create the second node and link the first node to the second one
+n2=Node("Mon")
+linkedList1.headValue.nextValue=n2
+#create the third node and link the second node to the third one
+n3=Node("Tues")
+n2.nextValue=n3
+
+linkedList1.printIt()
