d49e95ddb9f9c87476b0826bd1dcc75779e6a052
Youssef kafa <youssef.kafa19941@gmail.com>
2022-10-04 20:15:11 -0700

    second way of implementing a queue in python

--- DataStructures/queues.py
+++ DataStructures/queues.py
@@ -4,8 +4,27 @@ queue=[]
 queue.append(1)
 queue.append(2)
 queue.append(3)
-print(queue)
+#print(queue)
 queue.pop(0)
 queue.pop(0)
 queue.pop(0)
-print(queue)
+#print(queue)
+
+
+
+#second way of implementation using queue.deque
+from collections import deque
+queue=deque()
+queue.append(1)
+queue.append(2)
+queue.append(3)
+#print(queue) #outputs deque([1,2,3])
+queue.popleft()
+queue.popleft()
+#print(queue) #outputs deque([3])
+queue.popleft()
+#print(queue) #outputs deque([])
+
+
+
+
