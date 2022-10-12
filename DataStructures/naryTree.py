9373b26d777a0a950eedc6955f889613f7781cc3
Youssef kafa <youssef.kafa19941@gmail.com>
2022-10-12 12:46:17 -0700

    dfs on nary tree

--- DataStructures/naryTree.py
+++ DataStructures/naryTree.py
@@ -8,15 +8,34 @@ class Node :
 class NAryTree :
     def __init__(self) :
         self.root = None
-
+class Tools:
+    """
+    Given the root of an n-ary tree, return the preorder traversal (dfs) of its nodes' values.
+    Input: root = [1,null,3,2,4,null,5,6]
+    first node:1 and its children (3,2,4)
+    children of 3 are 5,6.
+Output: [1,3,5,6,2,4]
+    """
+    @staticmethod
+    def dfs(root:'Node') -> list[int]:
+        if root is None:
+            return []
+        stack,output=[root,],[]
+        while stack:
+            root=stack.pop()
+            output.append(root.value)
+            stack.extend(root.child[::-1])
+        return output
 if __name__=='__main__':
     tree=NAryTree()
     tree.root=Node(1)
-    tree.addChild(2)
-    tree.addChild(3)
-    tree.child[0].addChild(21)
-    tree.child[0].addChild(22)
-    tree.child[0].addChild(23)
-    tree.child[1].addChild(31)
-    tree.child[1].addChild(32)
-    tree.child[1].addChild(33)
+    tree.root.addChild(2)
+    tree.root.addChild(3)
+    tree.root.child[0].addChild(21)
+    tree.root.child[0].addChild(22)
+    tree.root.child[0].addChild(23)
+    tree.root.child[1].addChild(31)
+    tree.root.child[1].addChild(32)
+    tree.root.child[1].addChild(33)
+    print(Tools.dfs(tree.root))
+
