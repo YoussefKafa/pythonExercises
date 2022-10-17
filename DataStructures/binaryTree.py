ba95989d8bd8d39b3e9fd98c4d61118508519971
Youssef kafa <youssef.kafa19941@gmail.com>
2022-10-17 11:25:45 -0700

    binary tree bfs using queue

--- DataStructures/binaryTree.py
+++ DataStructures/binaryTree.py
@@ -1,3 +1,4 @@
+from collections import deque
 class Node:
    def __init__(self, val):
       self.left = None
@@ -23,6 +24,36 @@ class Tools:
                 helper(node.right, level + 1)
         helper(root, 0)
         return levels
+   @staticmethod
+   def levelOrderQueue( root):
+        levels = []
+        if not root:
+            return levels
+        
+        level = 0
+        queue = deque([root,])
+        while queue:
+            # start the current level
+            levels.append([])
+            # number of elements in the current level 
+            level_length = len(queue)
+            
+            for i in range(level_length):
+                node = queue.popleft()
+                # fulfill the current level
+                levels[level].append(node.val)
+                
+                # add child nodes of the current level
+                # in the queue for the next level
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            # go to next level
+            level += 1
+        
+        return levels
 if __name__ == '__main__':
     root=Node(1)
     root.left=Node(2)
@@ -30,3 +61,4 @@ if __name__ == '__main__':
     root.left.left=Node(4)
     root.left.right=Node(5)
     print(Tools.levelOrder(root))
+    print(Tools.levelOrderQueue(root))
