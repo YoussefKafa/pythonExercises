06ad96de93a07c71f6ae2ec3e8bb8518a11842e2
Youssef kafa <youssef.kafa19941@gmail.com>
2022-10-26 13:41:06 -0700

    comment

--- problems/reverseAstring.py
+++ problems/reverseAstring.py
@@ -1,3 +1,7 @@
+"""
+Write a function that reverses a string. The input string is given as an array of characters s.
+You must do this by modifying the input array in-place with O(1) extra memory.
+"""
 def reverseString(s: list[str]) -> None:
         i=0
         j=len(s)-1
