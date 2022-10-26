cdcc3bbf4db248fafe07c3ac2ccc03819198364b
Youssef kafa <youssef.kafa19941@gmail.com>
2022-10-26 13:34:30 -0700

    reverseAstring

---
+++ problems/reverseAstring.py
@@ -0,0 +1,13 @@
+def reverseString(s: list[str]) -> None:
+        i=0
+        j=len(s)-1
+        while i<j:
+            temp=s[i]
+            s[i]=s[j]
+            s[j]=temp
+            i+=1
+            j-=1
+if __name__=='__main__':
+    s=["H","a","n","n","a","h"]
+    reverseString(s)
+    print(s)
