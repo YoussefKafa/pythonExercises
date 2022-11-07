ae6b44d6929ed0e712cd0f60b9096eac6cd3fd8d
Youssef kafa <youssef.kafa19941@gmail.com>
2022-11-07 11:17:35 -0800

    calculating the running sum of 1D array in place

--- problems/PrefixSum/prefixSum.py
+++ problems/PrefixSum/prefixSum.py
@@ -3,3 +3,13 @@ def prefixSum(nums):
     for i in range(1,len(nums)):
         prefix.append(nums[i] + prefix[-1])
     return prefix
+
+#calculating prefix sum in place
+def runningSumOf1DArr(nums):
+    for i in range(1,len(nums)):
+        nums[i]+=nums[i-1]
+    return nums
+
+if __name__=='__main__':
+    print(prefixSum([1,2,3,4,5,6]))
+    print(runningSumOf1DArr([1,2,3,4,5,6]))
