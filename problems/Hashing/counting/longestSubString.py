ce7332e9d9ce5bb5157be5019f8bfee18ce4b7f3
Youssef kafa <youssef.kafa19941@gmail.com>
2022-11-15 12:31:49 -0800

    counting with sliding window

---
+++ problems/Hashing/counting/longestSubString.py
@@ -0,0 +1,23 @@
+"""
+You are given a string s and an integer k.
+Find the length of the longest substring that contains at most k distinct characters.
+For example, given s = "eceba" and k = 2, return 3.
+The longest substring with at most 2 distinct characters is "ece".
+"""
+
+from collections import defaultdict
+
+def find_longest_substring(s, k):
+    counts = defaultdict(int)
+    left = ans = 0
+    for right in range(len(s)):
+        counts[s[right]] += 1
+        while len(counts) > k:
+            counts[s[left]] -= 1
+            if counts[s[left]] == 0:
+                del counts[s[left]]
+            left += 1
+        
+        ans = max(ans, right - left + 1)
+    
+    return ans
