49bf2f6140c8405e7023b74f261150ddca7979dd
Youssef kafa <youssef.kafa19941@gmail.com>
2022-11-23 12:42:11 -0800

    one line solution for equal frequency problem

--- problems/Hashing/counting/equalFrequency.py
+++ problems/Hashing/counting/equalFrequency.py
@@ -5,12 +5,18 @@ For example, given s = "abacbc", return true. All characters appear twice.
  "a" appears 3 times, "b" appears 2 times. 3 != 2.
 """
 from collections import defaultdict
+from collections import Counter
 def equalFrequency(s:str) -> bool:
     counts=defaultdict(int)
     for c in s :
         counts[c]+=1
     frequencies=counts.values()
     return len(set(frequencies))==1
+
+
+#one line solution
+def areOccurrencesEqual(self, s: str) -> bool:
+    return len(set(Counter(s).values())) == 1
 
 if __name__=='__main__':
     print(equalFrequency("abacbc"))
