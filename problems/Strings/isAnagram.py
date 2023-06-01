e7b4720e6ec46e3e1416ba6e53f490b83e613006
Youssef kafa <youssef.kafa19941@gmail.com>
2023-06-01 09:54:35 -0700

    change folder name

---
+++ problems/Strings/isAnagram.py
@@ -0,0 +1,8 @@
+"""
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+"""
+def isAnagram(s: str, t: str) -> bool:
+    return sorted(s)==sorted(t)
+
---
+++ problems/Strings/needleInHaystack.py
@@ -0,0 +1,32 @@
+"""
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+"""
+
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        m = len(needle)
+        n = len(haystack)
+
+        for window_start in range(n - m + 1):
+            for i in range(m):
+                if needle[i] != haystack[window_start + i]:
+                    break
+                if i == m - 1:
+                    return window_start
+
+        return -1
