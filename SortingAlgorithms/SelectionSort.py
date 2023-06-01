5a51ce38d1c24ac8e3b3a1cb443d1df20c489e6f
Youssef kafa <youssef.kafa19941@gmail.com>
2023-06-01 10:48:55 -0700

    reversed selection sort

--- SortingAlgorithms/SelectionSort.py
+++ SortingAlgorithms/SelectionSort.py
@@ -6,7 +6,16 @@ def selectionSort(nums:list[int]) -> list[int]:
                 minIndex=j
         nums[i],nums[minIndex]=nums[minIndex],nums[i]
     return nums
-
+def reversedSelectionSort(nums:list[int]) -> list[int]:
+    for i in range(len(nums)):
+        largestIndex=i
+        for j in range(i+1,len(nums)):
+            if nums[j]>nums[largestIndex]:
+                largestIndex=j
+        nums[i],nums[largestIndex]=nums[largestIndex],nums[i]
+    return nums
 if __name__=='__main__':
     nums=[10,9,8,7,6,5,4,3,2,1]
     print(selectionSort(nums))
+    nums=[1,2,3,4,5,6]
+    print(reversedSelectionSort(nums))
